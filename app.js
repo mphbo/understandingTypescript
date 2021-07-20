4f9f5e36088c32aef73c5450d2d28b12784bb31a
Logan <logannormanthomas@protonmail.com>
2021-07-19 22:34:31 -0700

    working with object types

--- app.js
+++ app.js
@@ -1,15 +1,5 @@
-function add(n1, n2, showResult, phrase) {
-    var result = n1 + n2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else {
-        return result;
-    }
-}
-var number1 = 5;
-var number2 = 2.8;
-var printResult = true;
-var resultPhrase = "result is: ";
-var result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+var person = {
+    name: 'Maximilian',
+    age: 30
+};
+console.log(person.nickname);
--- app.ts
+++ app.ts
@@ -1,18 +1,6 @@
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+const person = {
+  name: 'Maximilian',
+  age: 30
+};
 
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
-  } else {
-    return result;
-  }
-}
-
-
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = "result is: ";
-
-const result = add(number1, number2, printResult, resultPhrase);
-console.log(result);
+console.log(person.nickname);
