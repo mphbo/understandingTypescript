d0696df73017c235c82ce90795ec9fbc6851de55
Logan <logannormanthomas@protonmail.com>
2021-07-19 23:38:59 -0700

    added more to typing with objects

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 const person = {
   name: 'Maximilian',
-  age: 30
+  age: 30,
+  hobbies: ['Sports', 'Cooking']
 };
 
-console.log(person.nickname);
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby);
+}
