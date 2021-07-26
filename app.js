64a7f7b2e49548f94623fd5864bf62dbb7a5de42
Logan <logannormanthomas@protonmail.com>
2021-07-26 00:26:57 -0700

    completed functions with typescript

--- app.js
+++ app.js
@@ -5,3 +5,14 @@ function printResult(num) {
     console.log('Result: ', +num);
 }
 printResult(add(5, 12));
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
+var combineValues;
+combineValues = add;
+// combineValues = printResult;
+console.log(combineValues(8, 8));
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
